c0347d7844593e5073956987dff2ac7b88aaa6c7
Zachery Sogolow <zsogolow@zacherys-air.lan>
2022-01-09 11:48:08 -0800

    add dotnet mvc

--- dotnet-web/run.sh
+++ dotnet-web/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm -p 8080:80  --name justforfunsies-dotnet-web -d zsogolow/justforfun:dotnet-web
+docker build . -t zsogolow/justforfun:dotnet-web -f ./docker/Dockerfile
+
+docker run --rm -p 8080:80  --name justforfunsies-dotnet-web zsogolow/justforfun:dotnet-web
